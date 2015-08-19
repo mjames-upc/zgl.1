22c80a2e15562948206465c1ff93e923222bb895
Prelang Builder <builder@prelang.com>
2015-08-19 00:49:20 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'ZoiglBeer' to allow for create, update

--- app/admin/zoigl_beer.rb
+++ app/admin/zoigl_beer.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ZoiglBeer do
+  permit_params :beername, :rbid, :rbbrewer, :alcohol, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
