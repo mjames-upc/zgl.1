32e5ee84db9d801b2c9fce02e654592014907847
Prelang Builder <builder@prelang.com>
2015-08-19 00:49:32 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :zoigl_stubes
+  resources :zoigl_stubes do
+  #->Prelang (voting/acts_as_votable)
+  member do
+    get "vote"
+  end
+end
+
 
   resources :zoigl_beers do
   #->Prelang (voting/acts_as_votable)
