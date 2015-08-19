99e23e525190982f55b815e78a022c209f02d802
Prelang Builder <builder@prelang.com>
2015-08-19 00:49:05 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'ZoiglKalendar' to allow for create, update

--- app/admin/zoigl_kalendar.rb
+++ app/admin/zoigl_kalendar.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ZoiglKalendar do
+  permit_params :begin, :end, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
